41cc6198cdd5fd2f938781f8030fe068d8dd79d4
kewonn2 <skgw1128@kaist.ac.kr>
2024-11-19 23:52:06 +0900

    [Add] Change files

--- publisher.py
+++ publisher.py
@@ -1,10 +1,22 @@
 import os
 from google.cloud import pubsub_v1
 
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\KKW\PycharmProjects\PubSubTest\key.json'
+os.environ['GOOGLE_CLOUD_PROJECT'] = 'aqueous-impact-441809-r5'
+os.environ['TOPIC_DATA_RECEIVED'] = 'data_received_IoT'
+
+
+print('Hi')
+print(os.getenv('GOOGLE_CLOUD_PROJECT'))
+print('Hi2')
+print(os.getenv('TOPIC_DATA_RECEIVED'))
+print('Hi3')
+
 publisher = pubsub_v1.PublisherClient()
 topic_name = 'projects/{project_id}/topics/{topic}'.format(
     project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-    topic='MY_TOPIC_NAME',  # Set this to something appropriate.
+    topic=os.getenv('TOPIC_DATA_RECEIVED'),  # Set this to something appropriate.
 )
 
 publisher.create_topic(name=topic_name)
--- subscriber.py
+++ subscriber.py
@@ -1,21 +1,40 @@
 import os
 from google.cloud import pubsub_v1
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\KKW\PycharmProjects\PubSubTest\key.json'
+os.environ['GOOGLE_CLOUD_PROJECT'] = 'aqueous-impact-441809-r5'
+os.environ['TOPIC_DATA_RECEIVED'] = 'data_received_IoT'
+os.environ['SUB_DATA_RECEIVED'] = 'data_received_IoT-sub'
+
 topic_name = 'projects/{project_id}/topics/{topic}'.format(
     project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-    topic='MY_TOPIC_NAME',  # Set this to something appropriate.
+    topic=os.getenv('TOPIC_DATA_RECEIVED'),  # Set this to something appropriate.
 )
 
 subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
     project_id=os.getenv('GOOGLE_CLOUD_PROJECT'),
-    sub='MY_SUBSCRIPTION_NAME',  # Set this to something appropriate.
+    sub=os.getenv('SUB_DATA_RECEIVED'),  # Set this to something appropriate.
 )
 
 def callback(message):
     print(message.data)
+    print("message received!")
+    #메세지를 수신했음을 Pub/Sub에 알림
     message.ack()
 
-with pubsub_v1.SubscriberClient() as subscriber:
-    subscriber.create_subscription(
-        name=subscription_name, topic=topic_name)
-    future = subscriber.subscribe(subscription_name, callback)
+project_id = os.getenv('GOOGLE_CLOUD_PROJECT')
+subscription_name = os.getenv('SUB_DATA_RECEIVED')
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_name)
+
+# Subscribe to the topic and wait for messages
+future = subscriber.subscribe(subscription_path, callback)
+
+
+print("Listening for messages on {}...".format(subscription_path))
+try:
+    # Keeps the main thread active and prevents it from exiting
+    future.result()
+except KeyboardInterrupt:
+    print("Stopped by user.")
+    future.cancel()  # Gracefully stop the subscription if interrupted by the user
